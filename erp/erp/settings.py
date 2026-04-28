68cd5e878466ee5644a7bfbfa0ad4cb768fbe433
이지훈 <yi10286@gmail.com>
2026-04-28 22:34:06 +0900

    fix: add DJANGO_SETTINGS_MODULE

--- erp/erp/settings.py
+++ erp/erp/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DJANGO_SETTINGS_MODULE = 'erp.erp.settings'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
